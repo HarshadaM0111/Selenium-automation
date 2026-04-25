9428a4eb46427879b913a30aa69fbdab19ee64cb
Harshada Mane <harshadamane0111@gmail.com>
2026-04-25 23:17:58 +0530

    Enhance comments in Test1.java for clarity
    Added comments to clarify the purpose of each action in the ChromeDriver usage.

--- Test1.java
+++ Test1.java
@@ -5,19 +5,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test1 {
 
 	public static void main(String[] args) {
-		
-		
+
+		// Launch browser initialize the ChromeDriver object
 		ChromeDriver driver = new ChromeDriver();
 		
 		//use to maximize the window 
-		driver.manage().window().maximize();
+		driver.manage().window().maximize(); 
 		
+		//used open to current webpage
 		driver.get("https://www.facebook.com/");
-		
+
+		//used for navigation website
 		driver.navigate().to("https://www.fortunecloudindia.com/");
-		
+
+		//used to navigate back
 		driver.navigate().back();
-		
+
+		//used to close browser
 		driver.close();
 	}
 
